55b9a5e18b6db4f1702f48737c5da30af1203fcd
muxianer <arivnren@renqiwen.local>
2015-07-20 20:05:20 +0800

    initialize

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by Arivn Ren on 15/7/20.
+//  Copyright (c) 2015年 Arivn Ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test/test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by Arivn Ren on 15/7/20.
+//  Copyright (c) 2015年 Arivn Ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
